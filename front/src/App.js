0c5edf4327a3f7f2b995d557e57d65a91e407635
Laura <l.maya10@uniandes.edu.co>
2019-09-14 12:28:36 -0500

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import Producto from "./Producto.js";
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      productos: []
+    }
+  }
+
+  componentDidMount()
+  {
+    fetch("/productos")
+      .then(res=> res.json())
+      .then(productos => this.setState({productos}));
+  }
+
+  renderProductos()
+  {
+    return this.state.productos.map(t => <Producto producto={t}></Producto>);
+  };
+
+  render() {
+  return(
+    <div className="container">
+      <div>HolaMundo</div>
+      Columna 1
+      <div className ="row">
+        {this.renderProductos()}
+      </div>
+    </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/productos", function(req, res, next) {
+  const productos = [
+    {id: 1, nombre: "Bolso1", precio:20000},
+    {id: 2, nombre: "Bolso2", precio:25000},
+    {id: 3, nombre: "Bolso3", precio:30000},
+  ];
+  res.json(productos);
+});
+
+module.exports = router;
